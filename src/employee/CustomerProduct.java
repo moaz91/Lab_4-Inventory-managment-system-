bd93ae5bd51855f8603b3e4cc8fe5a98fc8e713e
Ali1791 <alitarek13062005@gmail.com>
2025-10-16 18:23:18 +0300

    Update CustomerProduct.java

--- src/employee/CustomerProduct.java
+++ src/employee/CustomerProduct.java
@@ -1,5 +1,82 @@
-package employee;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inventory.mamagement.system;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;//dd-MM-yyyy
+
+
+/**
+ *
+ * @author AltAWKEl
+ */
 public class CustomerProduct {
+    //------------------------variables-------------------------------------------
+    private String customerSSN;
+    private String productID;
+    private LocalDate purchaseDate;
+    private boolean paid;
+//------------------------constructor------------------------------------------
+public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
+        this.customerSSN = customerSSN;
+        this.productID = productID;
+        this.purchaseDate = purchaseDate; 
 
+    }
+//------------------------getters-setters------------------------------------------
+//---------------------remove this part if you dont need it---------------------
+    public void setCustomerSSN(String customerSSN) {
+        this.customerSSN = customerSSN;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+//------------------------methods------------------------------------------
+public String getCustomerSSN(){
+return this.customerSSN;
+}
+public String getProductID(){
+return this.productID;
+}
+public LocalDate getPurchaseDate(){
+return this.purchaseDate;
+} 
+public String lineRepresentation(){
+return this.customerSSN+","+this.productID+","+this.purchaseDate+","+this.paid;
+} 
+public boolean isPaid(){
+return this.paid;
+}
+public void setPaid(boolean paid){
+this.paid=paid;
+}
+public String getSearchKey(){
+     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = this.purchaseDate.format(formatter);
+        return this.customerSSN+"," + this.productID+","+formattedDate;
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
