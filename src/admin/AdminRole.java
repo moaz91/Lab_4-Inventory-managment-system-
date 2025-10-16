342c7c8b329161b41c22ddf739fd65316662ba07
moaz91 <134746907+moaz91@users.noreply.github.com>
2025-10-16 18:08:36 +0300

    Initial commit

---
+++ src/admin/AdminRole.java
@@ -0,0 +1,40 @@
+package admin;
+
+import java.util.ArrayList;
+
+public class AdminRole {
+    private EmployeeUserDatabase database;
+
+    public AdminRole() {
+        database = new EmployeeUserDatabase("Employees.txt");
+    }
+
+    // ================ Accessors and Mutators ================
+    // To be editied in linking phase
+
+    public EmployeeUserDatabase getDatabase() {
+        return database;
+    }
+
+    // ================ Methods ================
+
+    public void addEmployee(String employeeId, String name, String email, String address, String phoneNumber) {
+        EmployeeUser newEmployee = new EmployeeUser(employeeId, name, email, address, phoneNumber);
+        database.insertRecord(newEmployee);
+    }
+
+    public EmployeeUser[] getListOfEmployees() {
+        ArrayList<EmployeeUser> list = database.returnAllRecords(); // Used ArrayList beacause it can change size
+                                                                    // dynamically and offers convenient methods
+                                                                    // (add,remove,search,filter)
+        return list.toArray(new EmployeeUser[0]);
+    }
+
+    public void removeEmployee(String key) {
+        database.deleteRecord(key);
+    }
+
+    public void logout() {
+        database.saveToFile();
+    }
+}
---
+++ src/employee/Product.java
@@ -0,0 +1,71 @@
+package employee;
+
+public class Product {
+    private String productID;
+    private String productName;
+    private String manufacturerName;
+    private String supplierName;
+    private int quantity;
+    private float price;
+
+    public Product(String productID, String productName, String manufacturerName,
+            String supplierName, int quantity, float price) {
+        this.productID = productID;
+        this.productName = productName;
+        this.manufacturerName = manufacturerName;
+        this.supplierName = supplierName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    // ================ Accessors and Mutators ================
+    // To be editied in linking phase
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getSearchKey() {
+        return productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getManufacturerName() {
+        return manufacturerName;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    // ================ Methods ================
+
+    public String lineRepresentation() { // Returns a comma-separated line of product data
+        return productID + "," + productName + "," + manufacturerName + "," +
+                supplierName + "," + quantity + "," + price;
+    }
+
+    // Optional methods
+    // To be edited in linking phase
+
+    public void sellUnit() {
+        if (quantity > 0) {
+            quantity--;
+        }
+    }
+
+    public void returnUnit() {
+        quantity++;
+    }
+}
