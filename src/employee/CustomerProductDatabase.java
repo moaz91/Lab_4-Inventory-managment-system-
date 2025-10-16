e7ab0cd4d540e87af0a3ec7ac71c8c1635cb44e5
nourk-soliman <es.nour.soliman2023@alexu.edu.eg>
2025-10-16 18:23:02 +0300

    Add files via upload

--- src/employee/CustomerProductDatabase.java
+++ src/employee/CustomerProductDatabase.java
@@ -2,127 +2,103 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package inventory.mamagement.system;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
+package lab4;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-
+import java.util.Scanner;
 /**
  *
- * @author AltAWKEl
+ * @author Nour
  */
-public class ProductDatabase extends Product  {
-//------------------------variables-------------------------------------------
+public class CustomerProductDatabase {
+    private ArrayList<CustomerProduct> records=new ArrayList<>();
+    private String fileName;
 
-    private ArrayList<Product> records;
-    private String filename;
-
-//------------------------constructor------------------------------------------
-    public ProductDatabase(String filename) {
-        this.filename = filename;
-                this.records = new ArrayList<>();//--------review.............
-
+    public CustomerProductDatabase(String fileName) {
+        this.fileName = fileName;
     }
-//------------------------getters-setters------------------------------------------
-public void setRecords(ArrayList<Product> records) {
-        this.records = records;
-    }
-    public String getFilename() {
-        return filename;
-    }
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
-//------------------------methods------------------------------------------
-
-//------------------------readFromFile------------------------------------------
-public void readFromFile() {
-        records.clear();
-        try (BufferedReader br = new BufferedReader(new FileReader(this.filename))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                Product p = createRecordFrom(line);
-                if (p != null) {
-                    records.add(p);
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("Error reading file ");
-        }
-    }
-//------------------------createRecordFrom------------------------------------------
-public Product createRecordFrom(String line) {
-        Product p=new Product();//--....................<review>_....................
-        String[] parts = line.split(",");
-        p.setproductID(parts[0].trim());
-        p.setproductName(parts[1].trim());
-        p.setmanufacturerName(parts[2].trim());
-        p.setsupplierName(parts[3].trim());
-        p.setquantity(Integer.parseInt(parts[4].trim()));
-        p.setprice(Float.parseFloat(parts[5].trim()));
- return p;
-    }
-//------------------------returnAllRecords------------------------------------------
-public ArrayList<Product> returnAllRecords() {
-      return records;   
-    }
-//------------------------contains------------------------------------------
-public boolean contains(String key) {
-        for (int i = 0; i < records.size(); i++) {
-        if (records.get(i).getSearchKey().equals(key)) {
-            return true;
-        }
-    }
-    return false;
-    }
-//------------------------getRecord------------------------------------------
-public Product getRecord(String key) {
-           for (int i = 0; i < records.size(); i++) {
-        if (records.get(i).getSearchKey().equals(key)) {
-            return records.get(i);
-                                  }
-                      }
-    return null;
-    }
-//------------------------insertRecord------------------------------------------
-public void insertRecord(Product record) {
-        if (!contains(record.getSearchKey())) {
-            records.add(record);
-        } else {
-            System.out.println("Product with ID " + record.getSearchKey() + " already exists.");
-        }
-    }
-//------------------------deleteRecord------------------------------------------
-public void deleteRecord(String key) {
-        Product target = null;
-         for (int i = 0; i < records.size(); i++) {
-        if (records.get(i).getSearchKey().equals(key)) {
-           target =records.get(i);
-           break;
-        }
-    }
-    if(target!=null)records.remove(target);
-     else System.out.println("No product found with ID: " + key);  
-    }
-//------------------------saveToFile------------------------------------------
- public void saveToFile() {
-     try (PrintWriter pw = new PrintWriter(new FileWriter(filename))) {
-            
-             for (int i = 0; i < records.size(); i++) {
-        Product p=records.get(i);
-         pw.println(p.lineRepresentation());
-                            } 
-        } catch (IOException e) {
-            System.out.println("Error writing to file");
-        }
-    }
-     
     
+    public void readFromFile()
+    {String line;// to read a line as a string from the file.
+    try{  
+    File file=new File(fileName);
+    Scanner scan=new Scanner(file);
+      while(scan.hasNextLine()) //While the file has a next line (will read until no new line is found).
+      {line=scan.nextLine();
+     if(null==createRecordFrom(line)) 
+     {break;} else {
+         records.add(createRecordFrom(line));
+        }
+          System.out.println(records);//I made a new toString method to override the one that produces a hashcode.
+      }
+      }
+      catch(FileNotFoundException e)
+      {System.out.println("File not found!");}}
+    
+    public CustomerProduct createRecordFrom(String line)
+    {String[]splitted;
+    String customerSSN,productID;
+    LocalDate purchaseDate;
+    boolean paid;
+     splitted=line.split(",");
+      if(splitted.length!=4) 
+      {System.out.println("The file is not written correctly.");//Not all fields are written.
+      return null;}
+      else{
+     customerSSN=splitted[0];
+     productID=splitted[1];
+     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");// The default LocalDate is YYYY/MM/DD, so we need to tell it to read the date in the form of dd-MM-yyyy
+     //also if you replace dd with DD, it will be wrong, beacuse DD would be the day of the year like 1-365. And YY means week based year.
+     purchaseDate=LocalDate.parse(splitted[2],formatter); 
+     paid=Boolean.parseBoolean(splitted[3]);
+     return new CustomerProduct(customerSSN,productID,purchaseDate,paid);}
+      }
+    public ArrayList<CustomerProduct> returnAllRecords()
+    {return records;}
+    
+public boolean contains(String key)
+{return getRecord(key) != null;}
+
+public CustomerProduct getRecord(String key)
+{
+  for(int i=0;i<records.size();i++)
+{String line= records.get(i).toSpecialString();// we convert the object in arraylist to string and then compare it with the key.
+if(line.equals(key)) {System.out.println("Found at index: "+i); return records.get(i);}}
+    System.out.println("Not Found."); return null;
 }
 
+public void insertRecord(CustomerProduct record)
+{if(!contains(record.toSpecialString()))
+    records.add(record);
+else System.out.println("The record is already there.");
+    System.out.println(records);}
+
+public void deleteRecord(String key)
+{CustomerProduct c=getRecord(key);
+if(c!=null) records.remove(c);
+    System.out.println(records);
+}
+
+public void saveToFile() throws IOException 
+{
+  
+    FileWriter writer=new FileWriter(fileName);
+    for(int i=0;i<records.size();i++)
+    { writer.write(records.get(i).toString());
+    if(i!=records.size()-1) writer.write("\n");} 
+    
+    
+    writer.close();
+    
+    
+        
+        }
+
+}
 
 
